7d69e9378810c06c4ff3d0f25f5593f24358a06c
PrateekGithub786 <75919841+PrateekGithub786@users.noreply.github.com>
2021-07-22 17:28:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+difference = 0;
+leftWristX = 0;
+rightWristX = 0;
+
 function setup(){
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -16,9 +20,17 @@ function modelLoaded(){
 function gotResults(results){
     if(results.length > 0){
         console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
     }
 }
 
 function draw(){
     background("#FF0000");
+    document.getElementById("value").innerHTML = "Width And The Height Of The Square Is " + difference;
+
+    textSize(difference);
+    stroke("#FFE787");
+    text("Prateek", 50, 400);
 }
